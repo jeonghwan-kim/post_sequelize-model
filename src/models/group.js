733595c37dbd34256b9f88d56237358300850407
김정환 <ej88ej@gmail.com>
2020-07-05 16:57:17 +0900

    init

---
+++ src/models/group.js
@@ -0,0 +1,14 @@
+const { Model, DataTypes } = require("sequelize");
+
+module.exports = class Group extends Model {
+  static initialize(sequelize) {
+    this.init({
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }, {
+      sequelize
+    })
+  }
+}
